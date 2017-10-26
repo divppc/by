8e17955c763b4e59c0e5323631597263a4d9efee
divppc <okal.ihor@gmail.com>
2017-10-26 20:22:22 +0300

    Header added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
 	
+	//mobile toggle function
+
+  $(".mobile-menu").on("click", function (e) {
+  	e.preventDefault();
+    $(this).toggleClass('active');
+    // $('.menu-line').toggleClass('active');
+  });
 });
