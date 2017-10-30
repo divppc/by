188fd8db18520cb5a86796f74c6a0c71a6b9da72
divppc <okal.ihor@gmail.com>
2017-10-30 20:59:35 +0200

    Sales and brands sections added

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,17 +9,123 @@ $(document).ready(function () {
   });
 
   // banners slider on main page
-  $('.main-slider').flickity({
-    contain: true,
-    watchCSS: true,
-    prevNextButtons: false
-  });
+  var mainSlider = $(".main-slider");
+
+  if (mainSlider.length > 0) {
+    $(mainSlider).slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      infinite: false,
+      mobileFirst: true,
+      dots: true,
+      dotsClass: 'dots',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: "unslick"
+        }
+      ]
+    });
+  };
 
   // advantages slider
-  $('.advantages-list').flickity({
-    cellAlign: 'right',
-    contain: true,
-    watchCSS: true,
-    prevNextButtons: false
-  });
+  var advantagesList = $(".advantages-list");
+
+  if (advantagesList.length > 0) {
+    $(advantagesList).slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      infinite: false,
+      mobileFirst: true,
+      dots: true,
+      dotsClass: 'dots',
+      responsive: [
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: "unslick"
+        }
+      ]
+    });
+  };
+
+
+  //carousel for items list (on mobiles only)
+  var itemsList = $(".items-list");
+
+  if (itemsList.length > 0) {
+    $(itemsList).slick({
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      arrows: false,
+      infinite: false,
+      mobileFirst: true,
+      dots: true,
+      dotsClass: 'dots',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 1200,
+          settings: "unslick"
+        }
+      ]
+    });
+  };
+
+  //brands carousel
+  var brands = $(".brands-list");
+
+  if (brands.length > 0) {
+    $(brands).slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      infinite: false,
+      mobileFirst: true,
+      dots: true,
+      dotsClass: 'dots',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: "unslick"
+        }
+      ]
+    });
+  };
 });
